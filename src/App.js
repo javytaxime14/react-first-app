dbc655b3d75f20efa6cd281cdaf3024d8658f4f0
Javiera Hidalgo <javiera_56@hotmail.com>
2022-03-19 02:30:46 -0300

    added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import SearchEngine from "./SearchEngine";
 
-
 export default function App() {
   return (
     <div className="App">
       <h1 className="mb-4"> ☀️ Let's talk about weather! ☁️</h1>
       <SearchEngine />
+        <p className="footer">
+          <a href="https://github.com/javytaxime14/react-first-app">
+          Open source project </a> by Javiera Hidalgo 
+        </p>
     </div>
   );
 }
